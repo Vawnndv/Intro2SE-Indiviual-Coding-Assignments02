21498f0b0e370b64515a2cea2f30400cd676f880
Vawnndv <dinhvanguyen810@gmail>
2022-11-09 17:16:36 +0700

    Test API and done

--- data.sql
+++ data.sql
@@ -13,3 +13,4 @@ INSERT INTO `products` VALUES ('2', 'Samsung S9', 'White', '24000000');
 INSERT INTO `products` VALUES ('3', 'Oppo F5', 'Red', '7000000');
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345';
+
